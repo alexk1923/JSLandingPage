b38de12277789336bcd96af42fa207087482e942
Alex Kullman <alexandrukullman@gmail.com>
2021-10-25 01:48:56 +0300

    Added updated message

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const date = document.querySelector('#date')
 const myname = document.querySelector('#name')
+const message = document.querySelector('#message')
 const playbutton = document.querySelector('#play-btn')
 const musicButton = document.querySelector('#music-btn')
 const musicList = document.querySelector('.music-container')
@@ -30,10 +31,23 @@ function addZeroes(x) {
  function updateBackground() {
     let today = new Date();
     let hours = parseInt(today.getHours());
-    if(hours > 12) {
+    if(hours > 0 && hours <= 7) {
+        const main = document.querySelector('.container')
+        main.style["background-color"] = "rgba(0, 0, 90, 0.6)"
+    }
+    if(hours > 7 && hours <= 12) {
+        const main = document.querySelector('.container')
+        main.style["background-color"] = "rgba(90, 0, 0, 0.4)"
+    }
+    if(hours > 12 && hours <= 18) {
+        const main = document.querySelector('.container')
+        main.style["background-color"] = "rgba(100, 50, 10, 0.5)"
+    }
+    if(hours > 18 && hours <= 0) {
         const main = document.querySelector('.container')
         main.style["background-color"] = "rgba(0, 0, 90, 0.4)"
     }
+    setTimeout(updateBackground, 60000);
  }
 
 function getName() {
@@ -55,6 +69,28 @@ function updateName(e) {
             this.blur();
         }
     }
+}
+
+function updateMessage() {
+    let today = new Date();
+    let hours = parseInt(today.getHours());
+    if(hours > 0 && hours <= 7) {
+       message.textContent = "Good night,";
+       console.log("cacatsdsdsds");
+    }
+    if(hours > 7 && hours <= 12) {
+       message.textContent = "Good morning, ";
+       console.log("cacatsdsdsds");
+    }
+    if(hours > 12 && hours <= 18) {
+       message.textContent = "Hello,";
+       console.log("cacatsdsdsds");
+    }
+    if(hours > 18 && hours <= 0) {
+        message.textContent = "Good evening, ";
+        console.log("cacatsdsdsds");
+    }
+    setTimeout(60000, updateMessage);
 }
 
 function playSong() {
@@ -93,9 +129,10 @@ function getAudio() {
 getTime();
 getName();
 updateBackground();
+updateMessage();
 
 myname.addEventListener('keypress', updateName);
 myname.addEventListener('blur', updateName);
 playbutton.addEventListener('click', playSong);
 musicButton.addEventListener('click', displayMenu);
-addButton.addEventListener('click', getAudio);
+// addButton.addEventListener('click', getAudio);
